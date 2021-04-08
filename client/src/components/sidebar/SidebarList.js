95c6bef48cc63604860ac9c084cda9025dc9af49
Jackie1645 <jackieli412@gmail.com>
2021-04-08 03:21:01 -0400

    Added moving list to top

--- client/src/components/sidebar/SidebarList.js
+++ client/src/components/sidebar/SidebarList.js
@@ -2,11 +2,15 @@ import React        from 'react';
 import SidebarEntry from './SidebarEntry';
 
 const SidebarList = (props) => {
+    let bot = props.todolists.filter((item) => item.id != props.activeid);
+    let active = props.todolists.find((item) => item.id === props.activeid);
+    active = active === undefined ? [] : [active];
+    bot = active.concat(bot);
     return (
         <>
             {
-                props.todolists &&
-                props.todolists.map(todo => (
+                bot &&
+                bot.map(todo => (
                     <SidebarEntry
                         handleSetActive={props.handleSetActive} activeid={props.activeid}
                         id={todo.id} key={todo.id} name={todo.name} _id={todo._id}
